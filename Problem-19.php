15e24b2ca1f8425eb098315eed9781fbc2ce786d
Md. Sabbir Rahman <sabbirrahman026@gmail.com>
2022-06-06 23:57:36 +0600

    Add files via upload

---
+++ Problem-19.php
@@ -0,0 +1,20 @@
+<?php
+
+// Write a PHP script to print "second" and Red from the following array. 
+
+// Sample Data :
+
+/*
+$color = array ( "color" => array ( "a" => "Red", "b" => "Green", "c" => "White"),
+"numbers" => array ( 1, 2, 3, 4, 5, 6 ),
+"holes" => array ( "First", 5 => "Second", "Third"));
+*/
+
+
+$color = array ( "color" => array ( "a" => "Red", "b" => "Green", "c" => "White"),
+"numbers" => array ( 1, 2, 3, 4, 5, 6 ),
+"holes" => array ( "First", 5 => "Second", "Third"));
+
+echo $color["holes"][5]."\n"; // prints "second"
+echo $color["color"]["a"]."\n"; // prints "Red"
+?>
